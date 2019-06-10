e70062f0323872707a3cd8323078118a7d8b539c
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-06-10 23:51:31 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,6 +32,8 @@ class MusicLibraryController
           list_songs_by_genre
         when "play song"
           play_song
+        when "list_artist"
+          list_songs_by_artist
         end
       end
     end
@@ -87,9 +89,4 @@ class MusicLibraryController
         puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
-  
-  def list_artist
-    list_songs_by_artist
-  end
-  
 end
